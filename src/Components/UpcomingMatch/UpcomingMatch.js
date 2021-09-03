d118a96a4f06bd8b0314da263eed4a8267e42260
Humayra Khanum <humayra.cse01eng@gmail.com>
2021-09-03 10:49:24 +0600

    daily cricket

--- src/Components/UpcomingMatch/UpcomingMatch.js
+++ src/Components/UpcomingMatch/UpcomingMatch.js
@@ -10,7 +10,7 @@ const UpcomingMatch = (props) => {
     const flagStyle = {height:'60px' ,borderRadius:'50%' } 
     const upcomingMatchStyle = {
     borderBottom:'1px solid grey', 
-    margin:'35px',padding:'5px',
+    marginLeft:'35px',padding:'5px',
     color:'rgb(71, 71, 71)',
     fontFamily: 'AustinNewstextRoman, Georgia, Times, serif',
     font:'Georgia'
@@ -21,7 +21,8 @@ const UpcomingMatch = (props) => {
     borderRadius: '50%',
     backgroundColor:'rgb(71, 71, 71)',
     zIndex:-'2',
-    position: 'absolute'
+    position: 'absolute',
+    marginTop:'30px',
     }
     const rectangleStyle ={
         height:'36px',
@@ -30,12 +31,11 @@ const UpcomingMatch = (props) => {
         borderRadius: '10%',
         zIndex:'-1',
         position: 'absolute',
-        marginTop:'10px',
+        marginTop:'39px',
         marginLeft: '5px',
         fontWeight:'bold',
         color: 'white',
-        textAlign:'enter',
-        fontSize: '10px'
+        fontSize: '10px',
     }
     // CSS style ends here 
     return (
@@ -46,7 +46,7 @@ const UpcomingMatch = (props) => {
             <p>UPCOMING</p>
             </div>
             </div>
-            <p style={{marginRight:'920px',marginTop:'67px',fontSize: '10px'}}>MATCH</p>
+            <p style={{marginRight:'917px',color:'black',display:'block',marginTop:'100px',fontSize:'10px'}}>MATCH</p>
             <p>{props.matchInfo.venue.name}, {location}</p>
             <p>{props.matchInfo.competition.datestart} , {timezone} Local time</p>
             <h4>{name} <img style={flagStyle} src={logo_url}  className ="flag" alt="country flag" /> <span style={{fontSize:'35px'}}>vs</span> <img style={flagStyle} src={props.matchInfo.teamb.logo_url} alt="country flag" /> {props.matchInfo.teamb.name} </h4>
